e9b3219d5e1a71c4df6407f68224b9784a557506
mykhailopulov <mihail.pulov.247@gmail.com>
2026-03-14 11:12:38 +0200

    test: success payments checked

--- src/test/java/OrderProcessorTest.java
+++ src/test/java/OrderProcessorTest.java
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import ua.univercity.*;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -24,4 +25,28 @@ class OrderProcessorTest {
     PaymentMethod payPal = amount -> {
         if (amount.getAmount() < 200L) throw new AppException("Fail");
     };
+
+    // Success card
+    @Test
+    void testSuccessCard() {
+        Order order = new Order("1", new OrderItem[]{new OrderItem("P1", new Money(500L))});
+        processor.process(order, cardPay);
+        assertEquals(OrderStatus.PAID, order.getStatus());
+    }
+
+    // Success discount
+    @Test
+    void testDiscount() {
+        Order order = new Order("2", new OrderItem[]{new OrderItem("P1", new Money(10000L))});
+        final long[] charge = {0L};
+        processor.process(order, amount -> charge[0] = amount.getAmount());
+        assertEquals(9500L, charge[0]);
+    }
+
+    // Success paypal
+    @Test
+    void testSuccessPayPal() {
+        Order order = new Order("4", new OrderItem[]{new OrderItem("P1", new Money(300L))});
+        assertDoesNotThrow(() -> processor.process(order, payPal));
+    }
 }
